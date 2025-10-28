ec721614d7f4555cbee6bc9285a8bdbd12399b01
Yara Emad Eldien <143430513+yaraemad4869@users.noreply.github.com>
2025-10-29 00:04:19 +0300

    UPD PASSWORD

--- LearningManagementSystem/Services/EmailService.cs
+++ LearningManagementSystem/Services/EmailService.cs
@@ -8,7 +8,7 @@ namespace LearningManagementSystem.Services
         private readonly string _smtpHost = "smtp.gmail.com"; 
         private readonly int _smtpPort = 465;
         private readonly string _senderEmail = "yara.emad4869@gmail.com"; 
-        private readonly string _senderPassword = "einp wvgz mgvc bisk"; 
+        private readonly string _senderPassword = "PASSWORD"; 
 
         public async Task SendEmailAsync(string recipientEmail, string subject, string body)
         {
